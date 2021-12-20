30c294a5f878f5ff0894e73547eb24bc85c1f63e
Ruiwen-T <you@example.com>
2021-12-20 13:18:33 -0500

    + SearchDriver

---
+++ SearchDriver.java
@@ -0,0 +1,11 @@
+public class SearchDriver{
+	public static double timeDiff(){
+		
+	}
+	public static void main(String[] args){
+		int[] test1 = new int[100];
+		for(int i : test1){
+		
+		}
+	}
+}
